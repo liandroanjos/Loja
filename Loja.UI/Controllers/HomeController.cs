cface860e008c89a60d80cfc4ea995c5249a964a
Liandro <liandro.freire@gmail.com>
2019-12-04 19:23:58 -0300

    commit

---
+++ Loja.UI/Controllers/HomeController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Loja.UI.Controllers
+{
+    public class HomeController: Controller
+    {
+        public string Index()
+        {
+            return "Nova Home";
+        }
+    }
+}
---
+++ Loja.UI/Controllers/ProdutoController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Loja.UI.Controllers
+{
+    public class ProdutoController: Controller
+    {
+        public string MinhaPagina()
+        {
+            return "Olá Mundo!";
+        }
+    }
+}
